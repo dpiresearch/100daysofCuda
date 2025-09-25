1af4241b8d12eed24bf3aae905fcad23fcbd60b5
Daniel Pang <dpang@Daniels-MacBook-Pro-2.local>
2025-09-25 13:26:10 -0700

    timings and shared mem

---
+++ src/day4/README.md
@@ -0,0 +1,19 @@
+# Matrix add two ways with shared memory and timings
+
+Matrix add implementation to illustrate that using x,y,and z does not impact performance and is only used for ease of impl or readability
+
+-- Output --
+Running in FUNCTIONAL mode...
+Compiling...
+Executing...
+Time taken cudamemcpy: 0.470821 ms
+Time taken matrixAdd_1D: 1519.588745 ms
+c[0]=0.000000 c[TOTAL-1]=32766.000000
+Time taken cudamemcpy first last: 0.879468 ms
+Time taken matrixAdd_1D_shared: 3506.687012 ms
+Exit status: 0
+
+-- questions --
+Why did shared mem take longer here?
+
+
---
+++ src/day4/matrix_add_timings.cu
@@ -0,0 +1,114 @@
+#include <cuda_runtime.h>
+#include <cstdio>
+
+__global__ void matrixAdd_1D(float *A, float *B, float *C, int m, int n) {
+    //__shared__ float shared_A[16][16];
+
+
+    const int total = m * n;
+
+    //if (dataidx < total) {
+    //    shared_A[threadIdx.x][threadIdx.y] = A[dataidx];
+    //}
+
+    //__syncthreads();
+    for (int i = 0; i < total; i++) {
+        C[i] = A[i] + B[i];
+    }
+}
+
+__global__ void matrixAdd_1D_shared(float *A, float *B, float *C, int m, int n) {
+
+    const int total = m*n;
+
+    extern __shared__ float shared_data_A[];
+    extern __shared__ float shared_data_B[];
+
+    for (int idx = 0; idx < total; idx++) {
+        shared_data_A[idx] = A[idx];
+        shared_data_B[idx] = B[idx];
+    }
+
+    __syncthreads();
+
+    for (int idx = 0; idx < total; idx++) {
+        C[idx] = shared_data_A[idx] + shared_data_B[idx];
+    }
+
+        
+}
+
+int main(int argc, char** argv) {
+    const int M = 128;
+    const int N = 128;
+    const int TOTAL = M * N;
+    int block_dim = 256;
+    int grid_dim = (TOTAL + block_dim - 1) / block_dim;
+
+    // host variables
+    float *A_h = (float*)malloc(TOTAL * sizeof(float));
+    float *B_h = (float*)malloc(TOTAL * sizeof(float));
+    float *C_h = (float*)malloc(TOTAL * sizeof(float));
+
+    // device variables
+    float *A_d, *B_d, *C_d;
+
+    cudaMalloc((void**)&A_d, TOTAL * sizeof(float));    
+    cudaMalloc((void**)&B_d, TOTAL * sizeof(float));
+    cudaMalloc((void**)&C_d, TOTAL * sizeof(float));
+    
+    // initialize host variables
+    for (int i = 0; i < TOTAL; i++) {
+        A_h[i] = i;
+        B_h[i] = i;
+    }  
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start, 0);
+    cudaEventSynchronize(start);
+
+    cudaMemcpy(A_d, A_h, TOTAL * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B_h, TOTAL * sizeof(float), cudaMemcpyHostToDevice);
+
+    cudaEventRecord(stop,0);
+    cudaEventSynchronize(stop);
+
+    float elapsed_time;
+    cudaEventElapsedTime(&elapsed_time, start, stop);
+    printf("Time taken cudamemcpy: %f ms\n", elapsed_time);
+
+    matrixAdd_1D<<<grid_dim, block_dim>>>(A_d, B_d, C_d, M, N);
+
+    cudaEventRecord(start, 0);
+    cudaEventSynchronize(start);
+
+    cudaEventElapsedTime(&elapsed_time, stop, start);
+    printf("Time taken matrixAdd_1D: %f ms\n", elapsed_time);
+
+    float first = 0.0f, last = 0.0f;
+
+    cudaMemcpy(&first, C_d, sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&last, C_d + TOTAL-1, sizeof(float), cudaMemcpyDeviceToHost);
+
+    printf("c[0]=%f c[TOTAL-1]=%f\n", first, last);
+
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+
+    cudaEventElapsedTime(&elapsed_time, start, stop);
+    printf("Time taken cudamemcpy first last: %f ms\n", elapsed_time);
+
+    int shared_mem_size = M*N*sizeof(float);
+
+    matrixAdd_1D_shared<<<grid_dim, block_dim, TOTAL*sizeof(float)>>>(A_d, B_d, C_d, M, N);
+
+    cudaEventRecord(start, 0);
+    cudaEventSynchronize(start);
+
+    cudaEventElapsedTime(&elapsed_time, stop, start);
+    printf("Time taken matrixAdd_1D_shared: %f ms\n", elapsed_time);
+
+}
